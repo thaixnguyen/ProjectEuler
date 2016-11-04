7c67a3adf79d0bfb70f8fe9fd75557adedc85d08
thaixnguyen <nguyet52@mcmaster.ca>
2016-11-04 02:29:54 -0400

    Add files via upload

---
+++ P5.java
@@ -0,0 +1,17 @@
+class P5{
+	public static void main(String args[]){
+		int primes[] = {2, 3, 5, 7, 11, 13, 17, 19};
+		int N = 20;
+		int a = 1;
+		int b = 1;
+		for (int i = 0; i < primes.length; i++){
+			b = 1;
+			while (b*primes[i] < N)
+				b *= primes[i];
+			a *= b;
+		}
+		System.out.println(a);
+
+	}
+
+}
