e61827b7326451a9ffabd691f3ba650f2efb43c6
Thai Nguyen <nguyet52@mcmaster.ca>
2016-11-05 16:18:50 -0400

    Update Problem12.java

--- Problem12.java
+++ Problem12.java
@@ -5,25 +5,25 @@ class P12{
 		for (int i = 0; i <= n; i++){
 			sum += i;
 		}
-		return sum;
+		return sum; //returns the sum of n*(n+1)/n which is the definition of a triangular number.
 	}
 
 	public static int Divisors(){
-		int counter = 0;
-		int n = 0;
-		int j = 0;
+		int counter = 0; //set a counter variable.
+		int n = 0; //set a variable equal to the triangular number.
+		int j = 0; //the variable used for the parameter of Triangular.
 		while (counter <= 500){
-			counter = 0;
-			j += 1;
-			n = Triangular(j);
-			for (int i = 1; i <= Math.sqrt(n); i++)
+			counter = 0; //resets the counter.
+			j += 1; //adds 1 to j everytime it loops around.
+			n = Triangular(j); 
+			for (int i = 1; i <= Math.sqrt(n); i++) //Used square root to reduce the amount of divisors checked.
 				if (n % i == 0){
-					counter += 1;
+					counter += 1; //adds 1 to the counter if it's a divisor of a triangular number.
 				}
 				
-			counter *= 2;
+			counter *= 2; //an observation I made was 1 to sqrt(n) is half of its divisors, so multiply by 2 to get the right number of divisors.
 		}
-		return n;
+		return n; //returns the first triangular number with 500 divisors.
 }
 
 	public static void main (String[] args){
