a081405dc6a87b40e4b10028907e5bce6f9e8a87
Thai Nguyen <nguyet52@mcmaster.ca>
2016-11-05 01:22:04 -0400

    Add files via upload

---
+++ P9.java
@@ -0,0 +1,19 @@
+public class P9{
+	private static final int N = 1000;
+	public static int ProjectEuler9(){
+		int prod = 0;
+		for (int a = 1; a < N; a++){
+			for (int b = a + 1; b < N; b++){
+				int c = 1000 - a - b;
+					if (a*a + b*b == c*c){
+						prod = a*b*c;
+					}
+			}
+	}
+		return prod;
+}
+
+	public static void main (String [] args){
+		System.out.println(ProjectEuler9());
+	}
+}
